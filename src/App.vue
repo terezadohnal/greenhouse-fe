5a04fa9aaa64ffba8f36ed754f796d9a8d8f3b3c
lenatodosijevic <xtodosij@mendelu.cz>
2024-03-19 14:13:48 +0100

    Navbar
    Created a component for navigation bar. Importing the component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <Navbar/>
     <v-main>
 		<router-view></router-view>
     </v-main>
@@ -7,8 +8,11 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
   name: 'App',
+  components: {Navbar},
 
   data () {
 	return {
