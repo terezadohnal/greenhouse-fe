a3f432961c37e1e026a7ef09fac6cbd615c3d23c
lenatodosijevic <xtodosij@mendelu.cz>
2024-04-02 14:13:24 +0200

    Signin Page
    Created a page for user registration. Defined rules for form validation.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import LinkView from "@/views/LinkView.vue";
 import LoginView from "@/views/LoginView.vue";
+import SigninView from "@/views/SigninView.vue";
 
 const routes = [
     {
@@ -10,14 +10,14 @@ const routes = [
         component: HomeView
     },
     {
-        path: "/link",
-        name: "link",
-        component: LinkView
-    },
-    {
         path: "/login",
         name: "login",
         component: LoginView
+    },
+    {
+        path: "/signin",
+        name: "signin",
+        component: SigninView
     }
 ]
 const router = createRouter({
