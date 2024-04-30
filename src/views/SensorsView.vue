ff2114fd2f3376ddc59cf768b1e2747f1ea8ab1c
Tereza Dohnalova <xdohna10@mendelu.cz>
2024-04-30 13:58:04 +0200

    fix: update sensors table

--- src/views/SensorsView.vue
+++ src/views/SensorsView.vue
@@ -19,15 +19,15 @@ export default {
   <v-container fluid>
     <v-row justify="center">
       <v-col cols="12" sm="8" md="6">
-        <v-card>
-          <v-card-title class="headline">Sensors</v-card-title>
-          <v-table height="200px">
+        <v-card elevation="4">
+          <v-card-title class="subtitle">Sensors</v-card-title>
+          <v-table height="220px" density="comfortable">
             <tbody>
-              <tr v-for="item in sensors" :key="item.label">
-                <td>{{ item.label }}:</td>
-                <td>{{ item.description }}</td>
-                <td class="edit-icon-cell">
-                  <v-icon @click="editItem(item)">mdi-pencil</v-icon>
+              <tr class="body_text" v-for="sensor in sensors" :key="sensor.id">
+                <td>{{ sensor.label }}</td>
+                <td>{{ sensor.description }}</td>
+                <td align="right" class="edit-icon-cell">
+                  <v-icon size="20" @click="editItem(item)">mdi-pencil</v-icon>
                 </td>
               </tr>
             </tbody>
