7bef271b95fec5db4f80988390e47701afcb17ca
Tereza Dohnalova <xdohna10@mendelu.cz>
2024-05-20 15:50:21 +0200

    feat: create filtering through measurements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,51 @@
 <template>
   <v-container fluid>
     <div class="filter_wrapper">
-      <v-container>
-        <v-menu open-on-click> <!-- Category filter-->
-          <template v-slot:activator="{ props }">
-            <v-btn variant="text" color="dark-green" v-bind="props" @click="toggleMenu('categoryMenuOpen')">
-              <span>{{ selectedCategory }}</span>
-              <v-icon :class="{ 'rotate-180': categoryMenuOpen }" class="ml-1">mdi-chevron-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item v-for="(category, index) in categories" :key="index"
-                         @click="selectItem(category, 'selectedCategory', 'categoryMenuOpen')">
-              <v-list-item-title>{{ category }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-
-        <v-menu open-on-click> <!-- Date filter-->
-          <template v-slot:activator="{ props }">
-            <v-btn variant="text" color="dark-green" v-bind="props" @click="toggleMenu('dateMenuOpen')">
-              <span>{{ selectedTimeFilter }}</span>
-              <v-icon :class="{ 'rotate-180': dateMenuOpen }" class="ml-1">mdi-chevron-down</v-icon>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item v-for="(time, index) in times" :key="index"
-                         @click="selectItem(time, 'selectedTimeFilter', 'dateMenuOpen')">
-              <v-list-item-title>{{ time }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-container>
+      <v-row>
+        <v-col md="3">
+          <!-- Category filter-->
+          <v-select
+            v-model="selectedCategory"
+            :items="categories"
+            label="Category"
+            item-title="text"
+            item-value="value"
+            variant="underlined"
+            hide-details
+            dense
+          />
+        </v-col>
+        <!-- Date filter-->
+        <v-col md="3">
+          <v-select
+            v-model="selectedTimeFilter"
+            :items="timeOrder"
+            label="Date"
+            item-title="text"
+            item-value="value"
+            variant="underlined"
+            dense
+          />
+        </v-col>
+      </v-row>
     </div>
-
-    <v-container> <!-- Start measure-->
+    <v-container>
+      <!-- Start measure-->
       <div class="new_measurement">
-        <v-menu open-on-click style="cursor: pointer;">
+        <v-menu open-on-click style="cursor: pointer">
           <template v-slot:activator="{ props }">
-            <v-btn color="dark-green" v-bind="props" @click="toggleMenu('measureMenuOpen')">
+            <v-btn
+              color="dark-green"
+              v-bind="props"
+              @click="toggleMenu('measureMenuOpen')"
+            >
               <span class="text-white"> Measure</span>
-              <v-icon color="white" :class="{ 'rotate-180': measureMenuOpen }" class="ml-1">mdi-chevron-down</v-icon>
+              <v-icon
+                color="white"
+                :class="{ 'rotate-180': measureMenuOpen }"
+                class="ml-1"
+                >mdi-chevron-down</v-icon
+              >
             </v-btn>
           </template>
           <v-list>
@@ -55,22 +60,32 @@
       </div>
     </v-container>
 
-    <v-row justify="center"> <!-- Table of measurements-->
+    <v-row justify="center">
+      <!-- Table of measurements-->
       <v-col cols="12" sm="8" md="6">
         <v-card elevation="4">
           <v-card-title class="headline">History of measurements</v-card-title>
           <v-table height="400px" density="comfortable">
             <tbody>
-            <tr v-for="measurement in measurements">
-              <td class="font-weight-bold">Measurement {{ measurement.order }}</td>
-              <td>{{ measurement.date }}</td>
-              <td align="right">
-                <measurement-detail :measurement="measurement"></measurement-detail>
-              </td>
-              <td align="center">
-                <v-icon @click="downloadMeasurement(item.label)">mdi-download</v-icon>
-              </td>
-            </tr>
+              <tr
+                v-for="measurement in filteredMeasurements"
+                :key="measurement.id"
+              >
+                <td class="font-weight-bold">
+                  Measurement {{ measurement.order }}
+                </td>
+                <td>{{ measurement.measure }}</td>
+                <td align="right">
+                  <measurement-detail
+                    :measurement="measurement"
+                  ></measurement-detail>
+                </td>
+                <td align="center">
+                  <v-icon @click="downloadMeasurement(item.label)"
+                    >mdi-download</v-icon
+                  >
+                </td>
+              </tr>
             </tbody>
           </v-table>
         </v-card>
@@ -80,55 +95,71 @@
 </template>
 
 <script>
-import NewMeasurementDialog from "@/components/NewMeasurementDialog.vue";
-import MeasurementDetail from "@/components/MeasurementDetail.vue";
-import {mapStores} from "pinia";
-import {useMeasurementStore} from "@/store/MeasurementStore";
+import NewMeasurementDialog from '@/components/NewMeasurementDialog.vue';
+import MeasurementDetail from '@/components/MeasurementDetail.vue';
+import { mapActions, mapState } from 'pinia';
+import { useMeasurementStore } from '@/store/MeasurementStore';
+import { categories, timeOrder } from '../helpers/constants';
 
 export default {
-  name: "HomeView",
-  computed: {
-    ...mapStores(useMeasurementStore),
-
-    measurements() {
-      return useMeasurementStore().measurements
-    }
+  name: 'HomeView',
+  components: {
+    MeasurementDetail,
+    NewMeasurementDialog,
   },
   data() {
     return {
       measureMenuOpen: false,
       categoryMenuOpen: false,
       categorySelected: false,
-      categories: ["All categories", "RGB camera", "Hyperspectral camera", "Acoustic Emission"],
-      selectedCategory: "All categories",
       dateMenuOpen: false,
       dateSelected: false,
-      times: ["From newest", "From oldest", "Custom period"],
-      selectedTimeFilter: "From newest",
+      categories,
+      selectedCategory: 'ALL',
+      timeOrder,
+      selectedTimeFilter: 'DESC',
     };
-
   },
 
-  components: {
-    MeasurementDetail, NewMeasurementDialog
-  },
+  computed: {
+    ...mapState(useMeasurementStore, ['measurements']),
 
-  created() {
-    useMeasurementStore().loadAll()
+    filteredMeasurements() {
+      let filtered = this.measurements;
+
+      // Filter by category
+      if (this.selectedCategory === 'RGB') {
+        filtered = filtered.filter((measurement) => measurement.isCamera);
+      } else if (this.selectedCategory === 'AE') {
+        filtered = filtered.filter((measurement) => measurement.isAcoustic);
+      }
+
+      // Sort by date (measure)
+      if (this.selectedTimeFilter === 'DESC') {
+        filtered = filtered.sort(
+          (a, b) => new Date(b.measure) - new Date(a.measure)
+        );
+      } else if (this.selectedTimeFilter === 'ASC') {
+        filtered = filtered.sort(
+          (a, b) => new Date(a.measure) - new Date(b.measure)
+        );
+      }
+
+      return filtered;
+    },
   },
 
   methods: {
+    ...mapActions(useMeasurementStore, ['loadAll']),
     toggleMenu(menu) {
       this[menu] = !this[menu];
     },
-    selectItem(item, selection, menu) {
-      this[selection] = item;
-      this[menu] = false;
-      this.categorySelected = true;
-    },
-  }
-}
+  },
+
+  async mounted() {
+    await this.loadAll();
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
